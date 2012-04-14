31ae8f3128647061360767852ccc8b032d472a9b
ksksue <ksksue@gmail.com>
2012-04-14 19:28:44 +0900

    NDKとJavaでRasterOperationの100回計測平均を出力

--- gen/jp/kobegtug/codesprint/ndkrasteroperation/R.java
+++ gen/jp/kobegtug/codesprint/ndkrasteroperation/R.java
@@ -11,12 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int chrome=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int imageviewMASK=0x7f050001;
-        public static final int imageviewRESULT=0x7f050002;
-        public static final int imageviewSRC=0x7f050000;
+        public static final int imageviewMASK=0x7f050002;
+        public static final int imageviewMASKJ=0x7f050005;
+        public static final int imageviewRESULT=0x7f050003;
+        public static final int imageviewRESULTJ=0x7f050006;
+        public static final int imageviewSRC=0x7f050001;
+        public static final int imageviewSRCJ=0x7f050004;
+        public static final int tvResultTime=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/jp/kobegtug/codesprint/ndkrasteroperation/NdkRasterOperationActivity.java
+++ src/jp/kobegtug/codesprint/ndkrasteroperation/NdkRasterOperationActivity.java
@@ -10,13 +10,15 @@ import android.graphics.Paint.Style;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class NdkRasterOperationActivity extends Activity {
     private static final String TAG = "NdkRasterOperationActivity";
+    private static final int MEASURE_TIMES = 100;
+    private TextView mResultTime;
     private ImageView mImageViewSRC;
     private ImageView mImageViewMASK;
     private ImageView mImageViewRESULT;
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,8 +26,10 @@ public class NdkRasterOperationActivity extends Activity {
         setContentView(R.layout.main);
 
         Bitmap bmp = BitmapFactory.decodeResource(getResources(),
-                R.drawable.ic_launcher);
-
+                R.drawable.chrome);
+//                R.drawable.ic_launcher);
+        long start,stop;
+        
         int width = bmp.getWidth();
         int height = bmp.getHeight();
 
@@ -47,18 +51,81 @@ public class NdkRasterOperationActivity extends Activity {
         int maskpixels[] = new int[width * height];
         mask.getPixels(maskpixels, 0, width, 0, 0, width, height);
 
-        raster(pixels, maskpixels, width, height, 1);   // 1:AND
+// -------------------------------------------------------------------------------
+// NDK Raster
+// -------------------------------------------------------------------------------
+        double totalTimeNDK = 0.0;
+        for(int i=0; i<MEASURE_TIMES; ++i) {
+        	bmp.getPixels(pixels, 0, width, 0, 0, width, height);
+        	start = System.nanoTime();
+        	raster(pixels, maskpixels, width, height, 1); // NDK Raster
+        	stop = System.nanoTime();
+        	totalTimeNDK += (stop-start)/1000.0; // us
+        }
+        double aveTimeNDK = totalTimeNDK/MEASURE_TIMES;
+// -------------------------------------------------------------------------------
 
         result.setPixels(pixels, 0, width, 0, 0, width, height);
 
+//        mResultTime = (TextView) findViewById(R.id.tvResultTime);
+//        mResultTime.setText("NDK Raster Time : " + (stop-start)/1000.0 + " us  ");
         mImageViewSRC = (ImageView) findViewById(R.id.imageviewSRC);
         mImageViewMASK = (ImageView) findViewById(R.id.imageviewMASK);
         mImageViewRESULT = (ImageView) findViewById(R.id.imageviewRESULT);
         mImageViewSRC.setImageBitmap(bmp);
         mImageViewMASK.setImageBitmap(mask);
         mImageViewRESULT.setImageBitmap(result);
+                
+// -------------------------------------------------------------------------------
+// Java Raster
+// -------------------------------------------------------------------------------
+        double totalTimeJava = 0.0;
+        for(int i=0; i<MEASURE_TIMES; ++i) {
+        	bmp.getPixels(pixels, 0, width, 0, 0, width, height);
+        	start = System.nanoTime();
+        	rasterJ(pixels, maskpixels, width, height, 1); // Java Raster
+        	stop = System.nanoTime();
+        	totalTimeJava += (stop-start)/1000.0; // us
+        }
+        double aveTimeJava = totalTimeJava/MEASURE_TIMES;
+// -------------------------------------------------------------------------------
+        
+        result.setPixels(pixels, 0, width, 0, 0, width, height);
+        
+        mResultTime = (TextView) findViewById(R.id.tvResultTime);
+        mResultTime.setText("Total Times : " + MEASURE_TIMES + "\nNDK Raster Time : " + aveTimeNDK + " us\nJava Raster Time : " + aveTimeJava + " us");
+        mImageViewSRC = (ImageView) findViewById(R.id.imageviewSRCJ);
+        mImageViewMASK = (ImageView) findViewById(R.id.imageviewMASKJ);
+        mImageViewRESULT = (ImageView) findViewById(R.id.imageviewRESULTJ);
+        mImageViewSRC.setImageBitmap(bmp);
+        mImageViewMASK.setImageBitmap(mask);
+        mImageViewRESULT.setImageBitmap(result);
+
     }
 
+    public void rasterJ(int[] src, int[] dst, int width, int height, int ope)
+    {
+    	for (int h=0; h<height;h++ ) {
+    		int ofst = h * width;
+    		for (int w=0;w<width;w++ ) {
+    			switch (ope) {
+    			case 0: // copy
+    				src[ofst+w] = dst[ofst+w];
+    				break;
+    			case 1: // and
+    				src[ofst+w] &= dst[ofst+w];
+    				break;
+    			case 2: // or
+    				src[ofst+w] |= dst[ofst+w];
+    				break;
+    			case 3: // xor
+    				src[ofst+w] ^= dst[ofst+w];
+    				break;
+    			}
+    		}
+    	}
+    }
+    
     public native int raster(int[] src, int[] dst, int width, int height,
             int ope);
 
